b8e6d1d2d9e547f7c631c8fd0d56f492af0761b9
Kanyawee <66025997@up.ac.th>
2025-01-15 10:06:26 +0700

    chart with Re-chart -RadialBarChart -SimpleBarChart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,4 +1,6 @@
 import UserCard from "@/components/UserCard";
+import CountChart from "@/components/CountChart";
+import AttendanceChart from "@/components/AttendanceChart";
 
 const AdminPage = () => {
     return (
@@ -16,9 +18,11 @@ const AdminPage = () => {
                 <div className="flex gap-4 flex-col lg:flex-row">
                     {/* COUNT CHART */}
                     <div className="w-full lg:w-1/3 h-[450px]">
+                        <CountChart />
                     </div>
                     {/* ATTENDANCE CHART */}
                     <div className="w-full lg:w-2/3 h-[450px]">
+                        <AttendanceChart />
                     </div>
                 </div>
                 {/* BOTTOM CHART */}

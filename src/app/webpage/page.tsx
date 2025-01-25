288fcb3b7d28d9f36f0e6ec15de297bd57d92722
66025009 <150766111+66025009@users.noreply.github.com>
2025-01-25 18:16:34 +0700

    first design 3D webpage

--- src/app/webpage/page.tsx
+++ src/app/webpage/page.tsx
@@ -1,7 +1,12 @@
-const Webpage = () => {
-    return (
-        <div className=''>Webpage wait for dev</div>
-    )
-}
+import React from 'react';
+import Spline from '@splinetool/react-spline';
 
-export default Webpage
+export default function webpage() {
+    return (
+        <div className="relative w-screen h-screen">
+            <div className="relative w-full h-full pointer-events-none">
+                <Spline scene="https://prod.spline.design/Y1lHTokbX9kL9nCm/scene.splinecode" />
+            </div>
+        </div>
+    );
+}

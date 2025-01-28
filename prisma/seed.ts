641133f27a2bd56f2f08ec045383f4ce7a840e87
Kanyawee <66025997@up.ac.th>
2025-01-28 16:45:49 +0700

    Update pagination settings, modify student results link, and adjust database schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -69,7 +69,6 @@ async function main() {
         sex: i % 2 === 0 ? UserSex.MALE : UserSex.FEMALE,
         subjects: { connect: [{ id: (i % 10) + 1 }] }, 
         classes: { connect: [{ id: (i % 6) + 1 }] }, 
-        birthday: new Date(new Date().setFullYear(new Date().getFullYear() - 30)),
       },
     });
   }
